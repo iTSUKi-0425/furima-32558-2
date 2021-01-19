3ab34a93871b99acb719ad76bbf0d9cf8b52c847
iTSUKi-0425 <75007909+iTSUKi-0425@users.noreply.github.com>
2021-01-19 13:36:14 +0900

    Basic認証を導入

---
+++ app/models/history.rb
@@ -0,0 +1,20 @@
+class History 
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id,:postal_code, :area_id, :city, :house_number, :building_name, :phone_number, :order_id, :token
+
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" }
+    validates :area_id, numericality: { other_than: 0, message: "Select" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "Input only number"}
+  end
+  
+  def save
+    # 各テーブルにデータを保存する処理を書く
+    order = Order.create(user_id: user_id, item_id:item_id)
+    Address.create(postal_code: postal_code, area_id: area_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
